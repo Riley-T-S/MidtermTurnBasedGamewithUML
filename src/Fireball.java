2bc71ed84a4c0d1568893544c2cc3d29c93089bc
fir1 <riley1016205@my.yosemite.edu>
2025-10-06 15:19:24 -0700

    Midterm Game: Update 10/6

--- src/Fireball.java
+++ src/Fireball.java
@@ -1,10 +1,16 @@
-public class Fireball extends Skills{
+import java.util.Random;
+
+public class Fireball extends Skills {
 
     //Components of Fireball
     private String name;
     private int damage;
     private int range;
     private int manaCost;
+    private int burnChance;
+
+    //Random generator for burn chance
+    private Random random = new Random();
 
     //Constructing Fireball
     Fireball(String name) {
@@ -13,11 +19,19 @@ public class Fireball extends Skills{
         this.damage = 20;
         this.range = 25;
         this.manaCost = 10;
+        this.burnChance = 30; //30% chance to Burn
     }
 
     //Allowing Fireball to be used
     public void use() {
         System.out.printf("%s is cast. It does %d damage, reaches %d ft, and uses %d mana.%n", name, damage, range, manaCost);
-    }
 
+
+        //Check to see if burn is applied
+        if (random.nextInt(100) < burnChance) {
+            System.out.println("The target is burned!");
+        } else {
+            System.out.println("The target is not burned.");
+        }
+    }
 }
